6e7e8e24415475aff16f6e06ea8377541bfb2992
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-06-30 19:46:57 +0000

    similar sample fix

--- tools/Dolphinv1.3/stepSummary.pl
+++ tools/Dolphinv1.3/stepSummary.pl
@@ -294,8 +294,8 @@ sub searchAligned
 				chomp(my $unmerged = `ls -d $outdir/tophat/pipe.tophat.$name*`);
 				my @unmerged_dirs = split(/[\n]+/, $unmerged);
 				foreach my $unmerge_dir (@unmerged_dirs){
-					print($unmerge_dir);
-					if (/$outdir\/tophat\/pipe\.tophat\.$name\_\d\d\// =~ $unmerge_dir) {
+					print $unmerge_dir + "\n";
+					if (/$outdir\/tophat\/pipe\.tophat\.$name\_[\d][\d]\// =~ $unmerge_dir) {
 						$merged_command+=" && " if ($merged_command ne "");
 						$merged_command+= "cat $unmerge_dir/align_summary.txt";
 					}
